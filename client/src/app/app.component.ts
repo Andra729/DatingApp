9650331f3ec0a55d0fe5eb1166bb1d02326068e8
Andra729 <szucsandras37@gmail.com>
2021-03-07 19:13:26 +0100

    end of section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Dating App';
+export class AppComponent implements OnInit {
+  title = 'Dating App is running!';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.http.get('https://localhost:5001/api/users')
+      // tslint:disable-next-line: deprecation
+      .subscribe(
+        response => {
+          this.users = response;
+        }, error => {
+          console.log(error);
+        });
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [
     HttpClientModule
